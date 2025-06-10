aa93ce4b2c76b0d2e7920b220d5c117cfc7016bf
Majid <majidhadi02@gmail.com>
2025-06-10 12:41:41 +0700

    Update backend Express project

--- config/database.js
+++ config/database.js
@@ -2,11 +2,11 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const poolEddyKalimantan = new Pool({
-  host: process.env.PGHOST,
-  port: process.env.PGPORT,
-  database: 'eddykalimantan',
-  user: process.env.PGUSER,
-  password: process.env.PGPASSWORD
+  host: process.env.PGHOST_EDDY,
+  port: process.env.PGPORT_EDDY,
+  database: process.env.PGDATABASE_EDDY,
+  user: process.env.PGUSER_EDDY,
+  password: process.env.PGPASSWORD_EDDY
 });
 
 const poolClimate = new Pool({
