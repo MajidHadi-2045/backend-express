82c856b6fb42ee20a869e6e8ad869dc927c660a4
Majid Solihin H. <144572765+MajidHadi-2045@users.noreply.github.com>
2025-06-12 14:59:01 +0700

    Update carbonPredictRoutes.js
    up

--- routes/carbonPredictRoutes.js
+++ routes/carbonPredictRoutes.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const ctl = require('../controllers/carbonPredictController');
+const ctl = require('../controllers/carbonPredictController');  // Pastikan import controller sudah benar
 
 // Simulasi tolerant
 router.get('/db/simulate', ctl.getSimulatedCO2);
 
 // 10 data terakhir prediksi CO2
-router.get('/db', ctl.getPredictLast10);
+router.get('/db', ctl.getCO2Last10);
 
 // Prediksi live dari backend Python
 router.get('/stream', ctl.getLivePrediction);
 
-// Downlaod data
+// Download data
 router.get('/download', ctl.downloadPredict);
 
 // Download by range date
